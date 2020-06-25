7444d3c0b806cab579af3c6877db816bdcbb13b8
Ghufran <ghufrankhan.gzk@gmail.com>
2020-06-25 12:40:14 +0530

    -extended Route Builder class in main application -transferred from from one folder to other -added Camel properties in application.properties

--- src/main/java/com/gzoul/springbootapachecamel/SpringbootApacheCamelApplication.java
+++ src/main/java/com/gzoul/springbootapachecamel/SpringbootApacheCamelApplication.java
@@ -1,13 +1,20 @@
 package com.gzoul.springbootapachecamel;
 
+import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApacheCamelApplication {
+public class SpringbootApacheCamelApplication extends RouteBuilder {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApacheCamelApplication.class, args);
 	}
 
+	@Override
+	public void configure() throws Exception {
+		System.out.println("Started");
+		from("file:C:/Users/Ghufr/OneDrive/Desktop/source1?noop=true").to("file:C:/Users/Ghufr/OneDrive/Desktop/target1");
+		System.out.println("Ended");
+	}
 }
